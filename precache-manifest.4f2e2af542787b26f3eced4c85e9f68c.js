1eb277075c9c0b1fa03016c6d63138328bcf3f3f
Coderchacha <toffees9596@gmail.com>
2021-10-12 18:11:57 +0900

    Updates

---
+++ precache-manifest.4f2e2af542787b26f3eced4c85e9f68c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25dac92932d60cd787eb13789da4ca66",
+    "url": "/portfolio-web/index.html"
+  },
+  {
+    "revision": "77f893de9b60e9b847cb",
+    "url": "/portfolio-web/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "8bae7ac7ac9d73dde2c1",
+    "url": "/portfolio-web/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "77f893de9b60e9b847cb",
+    "url": "/portfolio-web/static/js/2.2b74e184.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/portfolio-web/static/js/2.2b74e184.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bae7ac7ac9d73dde2c1",
+    "url": "/portfolio-web/static/js/main.326bdccb.chunk.js"
+  },
+  {
+    "revision": "9c052c639e89c193fefc",
+    "url": "/portfolio-web/static/js/runtime-main.fd62a409.js"
+  }
+]);
